a3cf8daaa51b2339d586a2e425613b01f815954e
jglaister <jeff.glaister@gmail.com>
2021-09-16 11:52:21 -0400

    Rename save_df to write_volumetrics

--- analyze_volumetrics/brain_volumetrics.py
+++ analyze_volumetrics/brain_volumetrics.py
@@ -19,7 +19,6 @@ class BrainVolumetrics():
                 search_path = os.path.join(rootdir, patient_id, scan_id,
                                            patient_id + '_' + scan_id + '*macruise_volumes.csv')
                 search_file = glob.glob(search_path)[0]
-                print(search_file)
                 if os.path.isfile(search_file):
                     filelist.append(search_file)
                 else:
@@ -43,7 +42,7 @@ class BrainVolumetrics():
             icv_file = os.path.join(os.path.split(f)[0],patient_id + '_' + scan_id + '_MPRAGEPre_reg_mask.nii.gz')
             if os.path.exists(icv_file):
                 icv = nib.load(icv_file).get_fdata()
-                icv_vol = np.sum(icv)*0.8*0.8*0.8
+                icv_vol = np.sum(icv)*0.8*0.8*0.8 #TODO: Read voxel size from image
                 new_df['ICV'] = icv_vol
             else:
                 print('No ICV in ' + filename)
@@ -53,7 +52,7 @@ class BrainVolumetrics():
             lesion_file = os.path.join(os.path.split(f)[0],patient_id + '_' + scan_id + '_MPRAGEPre_reg_durastrip_s3dl_lesions.nii.gz')
             if os.path.exists(lesion_file):
                 lesion = nib.load(lesion_file).get_fdata()
-                lesion_vol = np.sum(lesion)*0.8*0.8*0.8
+                lesion_vol = np.sum(lesion)*0.8*0.8*0.8 #TODO: Read voxel size from image
                 new_df['White Matter Lesion'] = lesion_vol
             else:
                 print('No lesion segmentation in ' + filename)
@@ -77,59 +76,7 @@ class BrainVolumetrics():
         merged_df.fillna(0)
         self.brain_volumetric_df = merged_df
 
-    def save_df(self, output_dir, output_prefix):
+    def write_volumetrics(self, output_dir, output_prefix):
         if self.brain_volumetric_df is not None:
             output_file = os.path.join(output_dir, output_prefix + '_MACRUISE_VOL.csv')
             self.brain_volumetric_df.to_csv(output_file)
-
-# path = "../RIS_ECTRIMS2021/*/*/*_macruise_volumes.csv"
-# all_files = sorted(glob.glob(path))
-#
-# # Grab MACruise results from each scan
-# all_df = []
-# filenames = []
-# for f in all_files:
-#     df = pd.read_csv(f, sep=',',header=0,index_col=1)
-#     filename = f.split('/')[-1]
-#     patient_id = filename.split('_')[0]
-#     scan_id = filename.split('_')[1]
-#     scan_name = patient_id + '_' + scan_id
-#     vol = df.transpose()[1:3]
-#     new_df = vol.rename(index={'Volume':scan_name, 'Name':'scan'})
-#     #ICV Mask
-#     icv_file = os.path.join(os.path.split(f)[0],patient_id + '_' + scan_id + '_MPRAGEPre_reg_mask.nii.gz')
-#     if os.path.exists(icv_file):
-#         icv = nib.load(icv_file).get_fdata()
-#         icv_vol = np.sum(icv)*0.8*0.8*0.8
-#         new_df['ICV'] = icv_vol
-#     else:
-#         print(filename)
-#         new_df['ICV'] = 'N/A'
-#     #Lesion
-#     lesion_file = os.path.join(os.path.split(f)[0],patient_id + '_' + scan_id + '_MPRAGEPre_reg_durastrip_s3dl_lesions.nii.gz')
-#     if os.path.exists(lesion_file):
-#         lesion = nib.load(lesion_file).get_fdata()
-#         lesion_vol = np.sum(lesion)*0.8*0.8*0.8
-#         new_df['White Matter Lesion'] = lesion_vol
-#     else:
-#         new_df['White Matter Lesion'] = 'N/A'
-#     all_df.append(new_df)
-#
-# merged_df = pd.concat(all_df, sort=False)
-# #merged_df.to_csv('/Users/jiwonoh/Desktop/LME_Scans/LME_macruise_vol.csv')
-# cols = merged_df.columns.tolist()
-#
-# cols = merged_df.columns.tolist()
-#
-# merged_df['Cerebral White Matter Volume'] = merged_df['Right Cerebral White Matter'] + merged_df['Left Cerebral White Matter']
-# merged_df['Cortical Grey Matter Volume'] = merged_df.iloc[:, cols.index('Right ACgG  anterior cingulate gyrus'):cols.index('Left TTG   transverse temporal gyrus')+1].sum(axis=1)
-# merged_df['Ventricular Volume'] = merged_df['3rd Ventricle'] + merged_df['Right Inf Lat Vent'] + merged_df['Left Inf Lat Vent'] + merged_df['Right Lateral Ventricle'] + merged_df['Left Lateral Ventricle']
-# merged_df['Brain Volume'] = merged_df.iloc[:, cols.index('3rd Ventricle'):cols.index('Left TTG   transverse temporal gyrus')+1].sum(axis=1)-merged_df['Ventricular Volume']
-#
-# #Reorder columns
-# cols = merged_df.columns.tolist()
-# cols = cols[cols.index('ICV'):] + cols[:cols.index('ICV')]
-# merged_df = merged_df[cols]
-# merged_df.fillna(0)
-#
-# merged_df.to_csv('RIS_ectrims_macruise_vol_july12.csv')
